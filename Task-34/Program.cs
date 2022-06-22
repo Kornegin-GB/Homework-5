0ff07469e3716bd3d5f72c017d059ed67ac8642e
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-22 21:25:25 +0300

    Решение задачи 34

--- Task-34/Program.cs
+++ Task-34/Program.cs
@@ -2,3 +2,35 @@
 
 // [345, 897, 568, 234] -> 2
 
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите размер массива: ");
+int size = Convert.ToInt32(ReadLine());
+int min = 100;
+int max = 999;
+
+int[] numberMass = new int[size];
+
+GetFillArray(numberMass, min, max);
+Write($"В массиве [{String.Join(", ", numberMass)}]");
+WriteLine($" четных чисел содержится {EvenNumbers(numberMass)}");
+
+void GetFillArray(int[] array, int randomMin, int randomMax)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      array[i] = new Random().Next(randomMin, randomMax + 1);
+   }
+}
+
+int EvenNumbers(int[] array)
+{
+   int n = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] % 2 == 0) n++;
+   }
+   return n;
+}
