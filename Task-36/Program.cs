c51c41f96aa2a79c29558411e2fd077853d7e323
Sergey Filippenkov <fsvmxd@yandex.ru>
2022-06-22 21:37:47 +0300

    Решение задачи 36

--- Task-36/Program.cs
+++ Task-36/Program.cs
@@ -2,3 +2,35 @@
 
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите размер массива: ");
+int size = Convert.ToInt32(ReadLine());
+int min = -100;
+int max = 100;
+
+int[] Mass = new int[size];
+
+GetFillArray(Mass, min, max);
+Write($"[{String.Join(", ", Mass)}] -> {SumNumberArray(Mass)}");
+
+void GetFillArray(int[] array, int randomMin, int randomMax) // Заполняем массив случайными числами
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      array[i] = new Random().Next(randomMin, randomMax + 1);
+   }
+}
+
+int SumNumberArray(int[] array) // Находим сумму чисел на нечетных позициях
+{
+   int sum = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (i % 2 != 0) sum += array[i];
+   }
+   return sum;
+}
